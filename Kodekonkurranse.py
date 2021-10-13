d261a3c57e0db12f45e6ea7cc610ccae384f1bd9
hbleie <92336221+hbleie@users.noreply.github.com>
2021-10-13 12:48:07 +0200

    Update Kodekonkurranse.py

--- Kodekonkurranse.py
+++ Kodekonkurranse.py
@@ -17,10 +17,7 @@ url = "https://api.opensea.io/api/v1/assets"
 
 sortering = s.selectbox("Sorter etter", ['Navn', 'Token ID', 'Antall salg'])
 
-if sortering == 'Antall salg':
-    respons = requests.get(url)
-    r = respons.json()
-    assets = sorted(r["assets"], key=itemgetter('num_sales'))
+def writeinfo (assets):
     for asset in assets:
         if asset['name']:
             s.header(asset['name'])
@@ -34,29 +31,26 @@ if sortering == 'Antall salg':
             s.video(asset['image_url'])
         elif asset['image_url'].endswith('svg'):
             s.write('Cannot display SVG-files')
+        elif asset['image_url'] == '':
+            s.write('No image')
         else:
             s.image(asset['image_url'])
+            
+
+if sortering == 'Antall salg':
+    respons = requests.get(url)
+    r = respons.json()
+    assets = sorted(r["assets"], key=itemgetter('num_sales'))
+    writeinfo(assets)
+  
             
 if sortering == 'Token ID':
     respons = requests.get(url)
     r = respons.json()
 
     assets = sorted(r["assets"], key = itemgetter('token_id'))
-    for asset in assets:
-        if asset['name']:
-            s.header(asset['name'])
-        else:
-            s.header(f"{asset['collection']['name']} #{asset['token_id']}")
-        s.write(asset['description'])
-        s.write("Token ID: " + asset['token_id'])
-        s.write("Antall salg: " + str(asset['num_sales']))
-        s.write("Pris: " + str(asset['transfer_fee']))
-        if asset['image_url'].endswith('mp4') or asset['image_url'].endswith('mov'):
-            s.video(asset['image_url'])
-        elif asset['image_url'].endswith('svg'):
-            s.write('Cannot display SVG-files')
-        else:
-            s.image(asset['image_url'])
+    writeinfo(assets)
+    
         
 if sortering == 'Navn':
     respons = requests.get(url)
@@ -66,21 +60,6 @@ if sortering == 'Navn':
         if not asset['name']:
             asset['name'] = 'unnamed'
     assets = sorted(r["assets"], key=itemgetter('name'))
-    for asset in assets:
-        if asset['name']:
-            s.header(asset['name'])
-        else:
-            s.header(f"{asset['collection']['name']} #{asset['token_id']}")
-        s.write(asset['description'])
-        s.write("Token ID: " + asset['token_id'])
-        s.write("Antall salg: " + str(asset['num_sales']))
-        s.write("Pris: " + str(asset['transfer_fee']))
-        if asset['image_url'].endswith('mp4') or asset['image_url'].endswith('mov'):
-            s.video(asset['image_url'])
-        elif asset['image_url'].endswith('svg'):
-            s.write('Cannot display SVG-files')
-        else:
-            s.image(asset['image_url'])
+    writeinfo(assets)
+    
             
-            
-
